40dadcd16232d02c4e0e2ad39f7a16e42bde1ba1
Andrey Dederchuk <adederchuk@icloud.com>
2024-10-30 21:08:09 +0300

    Добавить сортировку Датафрейма и проверить фильтры

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import module1 as mod
 
-N = 10
+N = 30
 
 dataSet = {'name': [],
       'birth year': [],
@@ -22,3 +22,33 @@ dataSet = mod.Name(dataSet, N)
 
 df = pd.DataFrame(dataSet)
 print(df)
+print(' ')
+
+df.to_csv('auto.csv', index= False, header= False, sep= ";")
+df.to_excel('auto.xlsx', index= False, header= False)
+
+df = df.sort_values('birth year', ascending= False)
+print(df)
+print(' ')
+
+df = df.sort_values('weight', ascending= False)
+print(df)
+print(' ')
+
+df = df.sort_values('height', ascending= False)
+print(df)
+print(' ')
+
+df = df.sort_values('weight', ascending= False)
+print(df)
+print(' ')
+
+df = df.sort_index()
+manAuto = df[df['sex'] == 'Мужской']
+womanAuto = df[df['sex'] == 'Женский']
+
+print(manAuto)
+print(' ')
+print(womanAuto)
+print(' ')
+print(df.loc[3])
